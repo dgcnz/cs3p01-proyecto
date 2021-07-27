6ec0aa8cad6a7d6f5ef9e00ce6999dc70ad6cade
Diego Canez <canezdiego@gmail.com>
2021-07-27 14:49:41 -0500

    Added parallel tsp

--- include/tsp/tsp.hpp
+++ include/tsp/tsp.hpp
@@ -3,6 +3,8 @@
 #ifndef CS3P01_PROYECTO_TSP_TSP_H
 #define CS3P01_PROYECTO_TSP_TSP_H
 
+#include <algorithm>
+#include <cstdio>
 #include <functional>
 #include <limits>
 #include <utility>
@@ -19,38 +21,87 @@ struct DenseGraph {
     std::vector<W> operator[](int u) const { return g[u]; }
 };
 
+template <typename W>
+struct Node {
+    int id;
+    int vis;
+    W cost;
+    Node* parent;
+    Node(int id, int vis, W cost, Node* parent) : id(id), vis(vis), cost(cost), parent(parent) {}
+};
+
+namespace sequential {
 template <typename W, W INF = std::numeric_limits<W>::max()>
-std::pair<std::vector<int>, W> tsp(DenseGraph<W, INF>& g, int src, int dst) {
+std::pair<std::vector<int>, W> tsp(DenseGraph<W, INF> g, int src, int dst) {
     int n = g.size();
-    std::vector<bool> vis(n, false);
-    W best_cost = INF;
-    W cost = W();
-    std::vector<int> order = {src};
     std::vector<int> best_order;
+    W best_cost = INF;
 
-    std::function<void(int)> bb = [&](int u) {
-        vis[u] = true;
+    std::function<void(Node<W>*)> bb = [&](Node<W>* u) {
         for (int v = 0; v < n; ++v) {
-            if (g[u][v] == INF or vis[v] or (v == dst and (int) order.size() + 1 != n)) continue;
-
-            if (cost + g[u][v] < best_cost) {
-                order.push_back(v);
-                cost += g[u][v];
-                bb(v);
-                order.pop_back();
-                cost -= g[u][v];
+            if (u->vis & (1 << v)) continue;
+            if (v == dst and __builtin_popcount(u->vis) + 1 != n) continue;
+            if (g[u->id][v] == INF) continue;
+            if (u->cost + g[u->id][v] > best_cost) continue;
+            bb(new Node<W>(v, u->vis | (1 << v), u->cost + g[u->id][v], u));
+        }
+        if (u->id == dst) {
+            Node<W>* temp = u;
+            best_order.clear();
+            while (temp) {
+                best_order.push_back(temp->id);
+                temp = temp->parent;
             }
+            best_cost = u->cost;
+            std::reverse(best_order.begin(), best_order.end());
         }
-        if ((int)order.size() == n) {
-            best_cost = cost;
-            best_order = order;
-        }
-        vis[u] = false;
+        delete u;
     };
 
-    bb(src);
+    bb(new Node<W>(src, (1 << src), 0, nullptr));
     return {best_order, best_cost};
 }
+}  // namespace sequential
+
+namespace parallel {
+template <typename W, W INF = std::numeric_limits<W>::max()>
+std::pair<std::vector<int>, W> tsp(DenseGraph<W, INF> g, int src, int dst) {
+    int n = g.size();
+    std::vector<int> best_order;
+    W best_cost = INF;
+
+    std::function<void(Node<W>*)> bb = [&](Node<W>* u) {
+#pragma omp taskloop default(none) shared(g, bb, u, best_order, best_cost) firstprivate(n, src, dst)
+        for (int v = 0; v < n; ++v) {
+            if (u->vis & (1 << v)) continue;
+            if (v == dst and __builtin_popcount(u->vis) + 1 != n) continue;
+            if (g[u->id][v] == INF) continue;
+            if (u->cost + g[u->id][v] > best_cost) continue;
+            bb(new Node<W>(v, u->vis | (1 << v), u->cost + g[u->id][v], u));
+        }
+        if (u->id == dst) {
+            Node<W>* temp = u;
+            std::vector<int> order;
+            while (temp) {
+                order.push_back(temp->id);
+                temp = temp->parent;
+            }
+            reverse(order.begin(), order.end());
+#pragma omp critical
+            {
+                best_cost = u->cost;
+                best_order = order;
+            }
+        }
+        delete u;
+    };
+
+#pragma omp parallel default(none) shared(g, bb, best_order, best_cost) firstprivate(n, src, dst)
+#pragma omp single nowait
+    { bb(new Node<W>(src, (1 << src), 0, nullptr)); }
+    return {best_order, best_cost};
+}
+}  // namespace parallel
 
 }  // namespace tsp
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,16 @@
 #include <omp.h>
 #include <stdio.h>
 
+#include <cassert>
 #include <iostream>
 
 #include "tsp/tsp.hpp"
 
+using namespace tsp;
+
 /// Program entry point.
 int main(const int argc, const char* argv[]) {
-    tsp::DenseGraph<int> g(5);
+    DenseGraph<int> g(5);
 
     g.add_edge(0, 1, 1);
     g.add_edge(1, 2, 1);
@@ -19,14 +22,17 @@ int main(const int argc, const char* argv[]) {
     g.add_edge(2, 3, 1);
     g.add_edge(3, 4, 1);
 
-    auto [order, cost] = tsp::tsp(g, 0, 4);
+    auto [order1, cost1] = sequential::tsp(g, 0, 4);
+    auto [order2, cost2] = parallel::tsp(g, 0, 4);
 
-    std::cout << cost << std::endl;
+    assert(order1 == order2);
+    assert(cost1 == cost2);
 
-#pragma omp parallel for
-    for (int i = 0; i < 5; ++i) {
-        printf("%d \n", i);
-    }
+
+    std::cout <<"COST: " << cost1 << std::endl;
+    std::cout << "ORDER: ";
+    for (auto x : order1) std::cout << x << " ";
+    std::cout << std::endl;
 
     return EXIT_SUCCESS;
 }
