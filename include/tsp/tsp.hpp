9179f80c5c7ff876c1a2a442097f3cf657edfb87
Diego Canez <canezdiego@gmail.com>
2021-07-27 16:38:43 -0500

    Added support for non-integral weights

--- include/tsp/tsp.hpp
+++ include/tsp/tsp.hpp
@@ -6,17 +6,21 @@
 #include <algorithm>
 #include <cstdio>
 #include <functional>
+#include <iostream>
 #include <limits>
 #include <utility>
 #include <vector>
 
 namespace tsp {
-template <typename W, W INF = std::numeric_limits<W>::max()>
+template <typename W>
 struct DenseGraph {
     std::vector<std::vector<W>> g;
-    DenseGraph(int n) : g(n, std::vector<int>(n, INF)) {}
+    W const INF;
+    DenseGraph(int n, W INF = std::numeric_limits<W>::max())
+        : g(n, std::vector<W>(n, INF)), INF(INF) {}
     void add_edge(int u, int v, W w) { g[u][v] = w; }
     int size() const { return g.size(); }
+    bool valid(int u, int v) { return g[u][v] != INF; }
     std::vector<W>& operator[](int u) { return g[u]; }
     std::vector<W> operator[](int u) const { return g[u]; }
 };
@@ -31,19 +35,19 @@ struct Node {
 };
 
 namespace sequential {
-template <typename W, W INF = std::numeric_limits<W>::max()>
-std::pair<std::vector<int>, W> tsp(DenseGraph<W, INF> g, int src, int dst) {
+template <typename W, typename BinOp = std::plus<W>>
+std::pair<std::vector<int>, W> tsp(DenseGraph<W> g, int src, int dst, BinOp add = std::plus<W>()) {
     int n = g.size();
     std::vector<int> best_order;
-    W best_cost = INF;
+    W best_cost = g.INF;
 
     std::function<void(Node<W>*)> bb = [&](Node<W>* u) {
         for (int v = 0; v < n; ++v) {
             if (u->vis & (1 << v)) continue;
             if (v == dst and __builtin_popcount(u->vis) + 1 != n) continue;
-            if (g[u->id][v] == INF) continue;
-            if (u->cost + g[u->id][v] > best_cost) continue;
-            bb(new Node<W>(v, u->vis | (1 << v), u->cost + g[u->id][v], u));
+            if (not g.valid(u->id, v)) continue;
+            if (add(u->cost, g[u->id][v]) > best_cost) continue;
+            bb(new Node<W>(v, u->vis | (1 << v), add(u->cost, g[u->id][v]), u));
         }
         if (u->id == dst) {
             Node<W>* temp = u;
@@ -58,26 +62,27 @@ std::pair<std::vector<int>, W> tsp(DenseGraph<W, INF> g, int src, int dst) {
         delete u;
     };
 
-    bb(new Node<W>(src, (1 << src), 0, nullptr));
+    bb(new Node<W>(src, (1 << src), W(), nullptr));
     return {best_order, best_cost};
 }
 }  // namespace sequential
 
 namespace parallel {
-template <typename W, W INF = std::numeric_limits<W>::max()>
-std::pair<std::vector<int>, W> tsp(DenseGraph<W, INF> g, int src, int dst) {
+template <typename W, typename BinOp = std::plus<W>>
+std::pair<std::vector<int>, W> tsp(DenseGraph<W> g, int src, int dst, BinOp add = std::plus<W>()) {
     int n = g.size();
     std::vector<int> best_order;
-    W best_cost = INF;
+    W best_cost = g.INF;
 
     std::function<void(Node<W>*)> bb = [&](Node<W>* u) {
-#pragma omp taskloop default(none) shared(g, bb, u, best_order, best_cost) firstprivate(n, src, dst)
+#pragma omp taskloop default(none) shared(g, bb, u, best_order, best_cost, add) \
+    firstprivate(n, src, dst)
         for (int v = 0; v < n; ++v) {
             if (u->vis & (1 << v)) continue;
             if (v == dst and __builtin_popcount(u->vis) + 1 != n) continue;
-            if (g[u->id][v] == INF) continue;
-            if (u->cost + g[u->id][v] > best_cost) continue;
-            bb(new Node<W>(v, u->vis | (1 << v), u->cost + g[u->id][v], u));
+            if (not g.valid(u->id, v)) continue;
+            if (add(u->cost, g[u->id][v]) > best_cost) continue;
+            bb(new Node<W>(v, u->vis | (1 << v), add(u->cost, g[u->id][v]), u));
         }
         if (u->id == dst) {
             Node<W>* temp = u;
@@ -98,9 +103,10 @@ std::pair<std::vector<int>, W> tsp(DenseGraph<W, INF> g, int src, int dst) {
         delete u;
     };
 
-#pragma omp parallel default(none) shared(g, bb, best_order, best_cost) firstprivate(n, src, dst)
+#pragma omp parallel default(none) shared(g, bb, best_order, best_cost, add) \
+    firstprivate(n, src, dst)
 #pragma omp single nowait
-    { bb(new Node<W>(src, (1 << src), 0, nullptr)); }
+    { bb(new Node<W>(src, (1 << src), W(), nullptr)); }
     return {best_order, best_cost};
 }
 }  // namespace parallel
--- test/test_tsp.cpp
+++ test/test_tsp.cpp
@@ -1,8 +1,10 @@
+#include <utility>
 #include <vector>
 
 #include "catch2/catch.hpp"
 #include "tsp/tsp.hpp"
 
+using namespace std;
 using namespace tsp;
 
 TEST_CASE("Sequential TSP") {
@@ -18,7 +20,7 @@ TEST_CASE("Sequential TSP") {
 
     auto [order_seq, cost_seq] = sequential::tsp(g, 0, 4);
 
-    REQUIRE(order_seq == std::vector<int>{0, 1, 3, 2, 4});
+    REQUIRE(order_seq == vector<int>{0, 1, 3, 2, 4});
     REQUIRE(cost_seq == 2);
 }
 
@@ -27,8 +29,10 @@ TEST_CASE("Sequential vs Parallel TSP") {
 
     g.add_edge(0, 1, 1);
     g.add_edge(1, 2, 1);
+    g.add_edge(1, 3, 0);
     g.add_edge(3, 2, 0);
     g.add_edge(2, 4, 1);
+    g.add_edge(2, 3, 1);
     g.add_edge(3, 4, 1);
 
     auto [order_seq, cost_seq] = sequential::tsp(g, 0, 4);
@@ -36,4 +40,27 @@ TEST_CASE("Sequential vs Parallel TSP") {
 
     REQUIRE(order_seq == order_par);
     REQUIRE(cost_seq == cost_par);
+}
+
+TEST_CASE("Sequential multiple parameter edges") {
+    using ii = pair<int, int>;
+    ii INF = make_pair(numeric_limits<int>::max(), numeric_limits<int>::max());
+
+    auto add_pair = [](ii a, ii b) { return make_pair(a.first + b.first, a.second + b.second); };
+
+    DenseGraph<ii> g(5, INF);
+
+    g.add_edge(0, 1, ii{1, 0});
+    g.add_edge(1, 2, ii{1, 0});
+    g.add_edge(1, 3, ii{0, 0});
+    g.add_edge(3, 2, ii{0, 0});
+    g.add_edge(2, 4, ii{1, 0});
+    g.add_edge(2, 3, ii{1, 0});
+    g.add_edge(3, 4, ii{1, 0});
+
+    auto [order_seq, cost_seq] = sequential::tsp(g, 0, 4, add_pair);
+    auto [order_par, cost_par] = parallel::tsp(g, 0, 4, add_pair);
+
+    REQUIRE(order_seq == vector<int>{0, 1, 3, 2, 4});
+    REQUIRE(cost_seq == ii{2, 0});
 }
